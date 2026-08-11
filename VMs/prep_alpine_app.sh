1da6c33b2ea8734afe7eeb0af0bcc807d513d1f1
alpeon <apetrovs@opennebula.io>
2026-08-11 22:52:56 +0300

    Add the fix variable block
    Signed-off-by: alpeon <apetrovs@opennebula.io>

--- VMs/prep_alpine_app.sh
+++ VMs/prep_alpine_app.sh
@@ -15,6 +15,11 @@ cd app
 mv app/* .
 pip install -r requirements.txt
 
+## Fix variables
+sed -i 's/DB_ROOT/DB_USER/g' app.py
+sed -i 's/MYSQL_ROOT_PASSWORD/DB_USER_PASSWORD/g' app.py
+sed -i 's/MYSQL_DATABASE/APP_DATABASE/g' app.py
+
 ## install cloudflared cli
 
 curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -o /usr/bin/cloudflared
